368eb6d1c8eb632ca4e7de769e960fc60200bf67
Michael Kelley <mikelley@microsoft.com>
2022-02-28 08:41:24 -0800

    arm64: hyperv: Enable Hyper-V synthetic clocks/timers
    This patch adds support for Hyper-V synthetic clocks and timers on
    ARM64. Upstream code assumes changes to Hyper-V that were made
    in Fall 2021 that fully virtualize the ARM64 architectural counter
    and timer so that the driver in drivers/clocksource/arm_arch_timer.c
    can be used.  But older versions of Hyper-V don't have this
    support and must use the Hyper-V synthetic clocks and timers.
    As such, this patch is out-of-tree code.
    
    This patch does two related things. First it splits the general
    Hyper-V initialization code to create hyperv_early_init() that runs
    much earlier during kernel boot. This early init function is needed
    so that core Hyper-V functionality is ready before the synthetic clocks
    and timers are initialized. Second, it adds Hyper-V clock and timer
    initialization via TIMER_ACPI_DECLARE() and hyperv_timer_init()
    in the Hyper-V clocksource driver in drivers/clocksource/hyperv_timer.c.
    
    Signed-off-by: Michael Kelley <mikelley@microsoft.com>
    [tyhicks: Forward port around a minor text conflict caused by commit
     245b993d8f6c ("clocksource: hyper-v: unexport __init-annotated
     hv_init_clocksource()")
    Signed-off-by: Tyler Hicks <tyhicks@linux.microsoft.com>
    [kms: Forward port to 6.1]
    Signed-off-by: Kelsey Steele <kelseysteele@microsoft.com>

--- drivers/clocksource/hyperv_timer.c
+++ drivers/clocksource/hyperv_timer.c
@@ -583,6 +583,20 @@ void __init hv_init_clocksource(void)
 	hv_setup_sched_clock(read_hv_sched_clock_msr);
 }
 
+/* Initialize everything on ARM64 */
+static int __init hyperv_timer_init(struct acpi_table_header *table)
+{
+	if (!hv_is_hyperv_initialized())
+		return -EINVAL;
+
+	hv_init_clocksource();
+	if (hv_stimer_alloc(true))
+		return -EINVAL;
+
+	return 0;
+}
+TIMER_ACPI_DECLARE(hyperv, ACPI_SIG_GTDT, hyperv_timer_init);
+
 void __init hv_remap_tsc_clocksource(void)
 {
 	if (!(ms_hyperv.features & HV_MSR_REFERENCE_TSC_AVAILABLE))
